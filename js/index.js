5c32024cf61ff0ee6ae9e180c39414b258b976de
Mr-Shoez <molelekishoez@gmail.com>
2026-03-23 03:42:42 +0200

    add initial files for basic calculator functionality

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/**
+===================
+Create 4 functions 
+===================
+1. addition(a, b)           --->  a + b;
+2. subtraction()            --->  a - b;
+3. multiplication()         --->  a * b;
+4. division()               --->  a / b;
+
+**/
